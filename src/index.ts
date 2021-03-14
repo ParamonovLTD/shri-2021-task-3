668ed5d690c24994cce069ecab402f95af3c9557
Roman Paramonov <paramonov.llc@gmail.com>
2021-03-14 20:29:54 +0300

    Изменения для запуска приложения

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,85 @@
-import { messageSetTheme, messageUpdate, XMessage } from './messages';
+import { messageSetTheme, messageUpdate, XMessage } from './messages'
 
 import {
-    actionMessage,
-    actionPrev,
-    actionNext,
-    actionRestart,
-    actionSetTheme,
-} from './application/actions';
-import { createState } from './application/state';
-import { createCurrentDataSelector, createCurrentIndexSelector, createProgressSelector, createThemeSelector } from './application/selectors';
-import { initIframe, initProgress, sendMessage, setElementTheme, setScale } from './application/view';
+  actionMessage,
+  actionPrev,
+  actionNext,
+  actionRestart,
+  actionSetTheme,
+} from './application/actions'
+import { createState } from './application/state'
+import {
+  createCurrentDataSelector,
+  createCurrentIndexSelector,
+  createProgressSelector,
+  createThemeSelector,
+} from './application/selectors'
+import {
+  initIframe,
+  initProgress,
+  sendMessage,
+  setElementTheme,
+  setScale,
+} from './application/view'
 
-import './index.css';
+import './index.css'
 
-import { stories } from './data';
+import { stories } from './data'
 
-const [dispatch, state$] = createState(stories);
+const [dispatch, state$] = createState(stories)
 
 function onMessage({ data }: MessageEvent<XMessage>) {
-    if (data.type === 'message@ACTION') {
-        dispatch(actionMessage(data.action, data.params));
-    }
+  if (data.type === 'message@ACTION') {
+    dispatch(actionMessage(data.action, data.params))
+  }
 }
 
-const player = document.querySelector<HTMLDivElement>('.player');
-const frames = stories.map(({ alias, data }) => initIframe(player, iframe => {
-    sendMessage(iframe, messageUpdate(alias, data));
-    iframe.contentWindow.addEventListener('message', onMessage);
-}));
+const player = document.querySelector<HTMLDivElement>('.player')! // Гарантирует, что элемент найден
+const frames = stories.map(({ alias, data }) =>
+  initIframe(player, iframe => {
+    sendMessage(iframe, messageUpdate(alias, data))
+    if (iframe.contentWindow) {
+      // Добавляю условие, чтобы !== null
+      iframe.contentWindow.addEventListener('message', onMessage)
+    }
+  })
+)
 
-const progress = document.querySelector<HTMLDivElement>('.progress-container');
-const bars = stories.map(() => initProgress(progress));
+const progress = document.querySelector<HTMLDivElement>('.progress-container')! // Гарантирует, что элемент найден
+const bars = stories.map(() => initProgress(progress))
 
-createProgressSelector(state$)
-    .subscribe(({ index, value }) => setScale(bars[index], value));
+createProgressSelector(state$).subscribe(({ index, value }) =>
+  setScale(bars[index], value)
+)
 
-createCurrentIndexSelector(state$)
-    .subscribe(index => {
-        player.style.transform = `translateX(-${index * 100}%)`;
-        bars.forEach((el, i) => setScale(el, i < index ? 1 : 0));
-    });
+createCurrentIndexSelector(state$).subscribe(index => {
+  player.style.transform = `translateX(-${index * 100}%)`
+  bars.forEach((el, i) => setScale(el, i < index ? 1 : 0))
+})
 
-createCurrentDataSelector(state$)
-    .subscribe(({ index, value: { alias, data } }) => {
-        sendMessage(frames[index], messageUpdate(alias, data));
-    });
+createCurrentDataSelector(state$).subscribe(
+  ({ index, value: { alias, data } }) => {
+    sendMessage(frames[index], messageUpdate(alias, data))
+  }
+)
 
-createThemeSelector(state$)
-    .subscribe(theme => {
-        setElementTheme(document.body, theme);
-        frames.forEach(iframe => sendMessage(iframe, messageSetTheme(theme)));
-    })
+createThemeSelector(state$).subscribe(theme => {
+  setElementTheme(document.body, theme)
+  frames.forEach(iframe => sendMessage(iframe, messageSetTheme(theme)))
+})
 
-document.querySelector<HTMLDivElement>('.set-light').addEventListener('click', () => dispatch(actionSetTheme('light')));
-document.querySelector<HTMLDivElement>('.set-dark').addEventListener('click', () => dispatch(actionSetTheme('dark')));
-document.querySelector<HTMLDivElement>('.prev').addEventListener('click', () => dispatch(actionPrev()));
-document.querySelector<HTMLDivElement>('.next').addEventListener('click', () => dispatch(actionPrev()));
-document.querySelector<HTMLDivElement>('.restart').addEventListener('click', () => dispatch(actionRestart()));
+document
+  .querySelector<HTMLDivElement>('.set-light')! // Гарантирует, что элемент найден
+  .addEventListener('click', () => dispatch(actionSetTheme('light')))
+document
+  .querySelector<HTMLDivElement>('.set-dark')! // Гарантирует, что элемент найден
+  .addEventListener('click', () => dispatch(actionSetTheme('dark')))
+document
+  .querySelector<HTMLDivElement>('.prev')! // Гарантирует, что элемент найден
+  .addEventListener('click', () => dispatch(actionPrev()))
+document
+  .querySelector<HTMLDivElement>('.next')! // Гарантирует, что элемент найден
+  .addEventListener('click', () => dispatch(actionNext())) // Вызывалось actionPrev
+document
+  .querySelector<HTMLDivElement>('.restart')! // Гарантирует, что элемент найден
+  .addEventListener('click', () => dispatch(actionRestart()))
